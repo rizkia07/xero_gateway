8019d1ce9fd1f896a2e24425644394372b37c5d8
hayakaza <hayhay2301@gmail.com>
2013-08-14 12:25:12 +0700

    get Organisation example

--- examples/organisation_api.rb
+++ examples/organisation_api.rb
@@ -9,14 +9,20 @@ XERO_KEYS = YAML.load_file(File.dirname(__FILE__) + '/xero_keys.yml')
 gateway = XeroGateway::Gateway.new(XERO_KEYS["xero_consumer_key"], XERO_KEYS["xero_consumer_secret"])
 
 # authorize in browser specific to payroll-API
-%x(chromium-browser #{gateway.request_token.authorize_url})
+%x(open #{gateway.request_token.authorize_url})
 
-puts "Enter the verification code from Xero?"
+pp "Enter the verification code from Xero?"
 oauth_verifier = gets.chomp
 
 gateway.authorize_from_request(gateway.request_token.token, gateway.request_token.secret, :oauth_verifier => oauth_verifier)
 
+
+pp "**** Get Organisation"
 responses = gateway.get_organisation
+
+pp "**** XML FROM XERO"
 puts responses.response_xml
+
+pp "************** ORGANISATION DETAIL"
 organisation = responses.response_item
 pp organisation.inspect
